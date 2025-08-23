9055541aeb537b392d522506e991f2f3c44dd9e9
saracgarcia3 <saracgarcia3@gmail.com>
2025-08-23 08:42:49 -0500

    Cambios 2 punto

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -2,13 +2,20 @@ package edu.eci.arsw.primefinder;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 100_000_000);
-		
-		pft.start();
-		
-		
-	}
-	
+    public static void main(String[] args) {
+
+        int limite = 100_000_000;
+        int parte = limite / 3; 
+
+        
+        PrimeFinderThread hilo1 = new PrimeFinderThread(0, parte);
+        PrimeFinderThread hilo2 = new PrimeFinderThread(parte, 2 * parte);
+        PrimeFinderThread hilo3 = new PrimeFinderThread(2 * parte, limite);
+
+        
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+    }
+
 }
